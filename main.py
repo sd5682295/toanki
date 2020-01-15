6bf86ff869154cfb5c154a5c845b3058ba4c5bab
sd5682295 <181673940@qq.com>
2020-01-15 23:48:49 +0800

    删除多余的anki_test.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # encoding = utf-8
-from demo2.tool.dir_handle import dir_handle
-from demo2.tool.run_html import run_html
+from tool.dir_handle import dir_handle
+from tool.run_html import run_html
 
 if __name__ == "__main__":
     dh = dir_handle()
