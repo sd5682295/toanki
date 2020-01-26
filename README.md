005e8b3bc1a0d4118722c0283f4a39db07a2490d
sd5682295 <181673940@qq.com>
2020-01-27 00:10:24 +0800

    更新readme.md

--- README.md
+++ README.md
@@ -1,67 +1,78 @@
 # About
+做此项目是为了将使用[幕布](https://mubu.com/)网做的结构化笔记自动转换成[anki](https://zhuanlan.zhihu.com/-anki)的问答卡片，后续将出于人性化操作进行优化。由于是个人使用工具，所以不做性能上的优化和强化，或是延后再实现性能优化。
 
-做此项目是为了将使用幕布网做的结构化笔记自动转换成anki的问答卡片
+使用幕布可以结构化处理笔记，可以快速将传统笔记优化成大纲格式，使用toanki可以自动将大纲格式的笔记转化成记忆卡片，并且可以自己选择记忆的内容，并可以通过增量笔记的方式增量记忆卡片方便管理，同时有的记忆卡片忘记它的背景知识也可以查询幕布笔记。
 
-
-# 说明
-
-现在有3个demo
-demo_prototype是最早的，通过jupyter运行
-使用方法：
-> 1) 先从幕布导出html文件放在demo_prototype根目录下
-> 2) 使用jupyter打开toanki.ipynb，第一行设置输入的html文件地址，最后一行设置输出的txt文件地址，从上往下运行，即可完成转换
-
-demo 是将demo_prototype文件转换成.py文件,用法和demo_prototype一致
-使用方法：
-> 1) 先从幕布导出html文件放在demo根目录下
-> 2) 使用编辑器打开toanki.py，第一行设置输入的html文件地址，最后一行设置输出的txt文件地址，保存后运行toanki.py即可
-
-demo2  是最新版分离了进出端
-使用方法：
-> 1) 先从幕布导出html文件放在demo2根目录下的html目录中
-> 2) 运行main.py,新生产的文件txt文件出现在csv目录中
+在这个工具组合中toanki作为粘合剂，致力于实现这2个工具间转换的最佳实现，并使这套工具链变为学习复习的最佳工具组合
 
 
+# 打赏
+为了支持作者继续优化和维护这个工具请大家多多给予打赏。
+
+如果有其他作者在我的工具基础上开发新的分支，可以有2钟处理方式，1：贴本文中的收款二维码，2：收取打赏后分一半给作者，既通过本文中的收款二维码打赏。如用作商业用途，需联系作者，联系方式:181673940@qq.com
 
 
-## 技术栈
+![图片](https://uploader.shimo.im/f/GUDdIShehwENTpzj.png!thumbnail)![图片](https://uploader.shimo.im/f/R9DdWxAOecMwf5y3.png!thumbnail)
 
+# # 使用方法：
+
+```
+git clone https://github.com/sd5682295/toanki.git 
+```
+先将需要处理的幕布笔记以html格式导出，然后放到根目录下的html文件夹下
+
+```
+cd toanki
+python main.py
+```
+
+
+# 技术栈
 python 3.7
 
 
 
 # Todo list
-
 - [x] .ipynb格式的demo -- 完成
 - [x] 转换成.py格式demo -- 完成
-- [x] 分装输入输出端 -- 完成 
+- [x] 分装输入输出端 -- 完成
 - [x] 各功能进行封装 -- 完成
 - [ ] 将转化完成的html文件移除 -- 正在做
+- [ ] 通过不同颜色判断是否需要正反导入两次
 - [ ] 完善测试
 - [ ] 以后再说
-
-
-# License 
-
+# License
 [MIT](https://opensource.org/licenses/MIT)
-Copyright (c) 2020 sd5682295
-------------------------------------------------------------------
+
+## Copyright (c) 2020 sd5682295
 Permission is hereby granted, free of charge, to any person obtaining a copy
+
 of this software and associated documentation files (the "Software"), to deal
+
 in the Software without restriction, including without limitation the rights
+
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+
 copies of the Software, and to permit persons to whom the Software is
+
 furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
+
 copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
 SOFTWARE.
 
 
--- test/test_qa.py
+++ test/test_qa.py
@@ -6,6 +6,7 @@ from tool.qa import qa
 
 
 class QaCase(unittest.TestCase):
+    count_tag = False
     def setUp(self):
         with open('./html/测试方法和流程拆分出的内容.html', 'r', encoding='utf-8') as f:
             html_data = f.read()
@@ -15,71 +16,39 @@ class QaCase(unittest.TestCase):
         datas = html_data.xpath('//span[@color="#3da8f5"]')
         self.datas = [qa(data) for data in datas]
 
+    def log_handle(self, info):
+        if QaCase.count_tag == False:
+            QaCase.count_tag = 0
+        print('{}{}'.format(str(QaCase.count_tag).zfill(3), info))
+        QaCase.count_tag += 1
+
     def test_get_obj(self):
-        # print(self.test_get_obj().doc())
+        self.log_handle('qa_get_obj测试返回锚点对象是否成功')
         for i in self.datas:
             self.assertEqual(i.get_obj(), i.obj)
 
-
     def test_make_pre(self):
+        self.log_handle('qa_make_pre测试获取锚点上级有效目录是否成功')
         self.assertEqual(self.datas[0].make_pre(), '', '测试第一个目录的上级目录是不是为空')
         self.assertEqual(self.datas[10].make_pre(), '[如何解决没有合适的正交表？--k值不合适]', '测试第10个目录的上级目录是不是"[如何解决没有合适的正交表？--k值不合适]"')
 
     def test_img_handle(self):
-        """
-        处理图片地址，将https改成http
-        :param img: 图片地址
-        :return: 将https改成http后的图片地址 <ip>
-        """
+        self.log_handle('qa_img_handle测试获图片地址的https转换成http是否有效')
         self.assertEqual(self.datas[0].img_handle('https://baidu.com'), 'http://baidu.com')
 
-    #     img[0].replace('https','http')
 
     def test_have_children(self):
-        """
-        判断是否有子目录
-        :return: 返回判断值 <bool>
-        """
+        self.log_handle('qa_have_children测试判断是否有子目录是否有效')
+
         self.assertTrue(self.datas[9].have_children())
         self.assertTrue(self.datas[0].have_children())
 
-    # def have_title_img(self):
-    #     """
-    #     判断是否有标题图片
-    #     :return: 返回判断值 <bool>
-    #     """
-    #     return len(self.get_title_img())
-    #
-    # def get_img(self, obj):
-    #     """
-    #     获取图片
-    #     :param obj: xpath节点
-    #     :return: 返回节点的图片地址<ip>
-    #     """
-    #     return self.img_handle(obj.xpath('../div/img/@src'))
-    #
-    # def get_title_img(self):
-    #     """1
-    #     获取qa的title图片
-    #     :return: 获取图片地址<ip>
-    #     """
-    #     return self.get_img(self.obj)
 
     def test_title(self):
-        """
-        获取节点标题
-        :return: 节点标题<html>
-        """
+        print('测试文档锚点的内容获取是否有效')
         self.assertEqual(self.datas[0].title(), '<p>因果图法说明</p>')
         self.assertEqual(self.datas[10].title(), '<p>m值不合适</p>')
-        # if self.have_children():
-        #     if self.get_title_img():
-        #         return '<p>{}</p><img src={} />'.format(self.obj.xpath('./text()')[0].replace(' ', ''),
-        #                                                 self.get_title_img()[0])
-        #     else:
-        #         return '<p>{}</p>'.format(self.obj.xpath('./text()')[0].replace(' ', ''))
-        # my_data = self.obj.xpath('./text()')
-        # return '<p>{}</p>'.format(my_data[0].replace(' ', ''))
+
 
     def test_detail(self):
         """
@@ -92,16 +61,8 @@ class QaCase(unittest.TestCase):
         # print([data.detail for data in self.datas])
         self.assertEqual(self.datas[0].detail(), '<p>1、因：输入条件</p><p>2、果：输出结果</p><p>3、以画图的方式来表示输入条件和输出结果之间的关系</p>')
         self.assertEqual(self.datas[10].detail(), '<p>1、最大值</p><p>2、少数服从多数</p>')
-        # img = self.get_title_img()
-        # if not self.have_children():
-        #     return '<img src={} />'.format(img)
-        # detail_text = self.obj.xpath('../ul/li/span/text()')
-        # if img:
-        #     return ('<p>{}</p><img src={} />'.format(detail_text, img))
-        # else:
-        #     text = '</p><p>'.join(detail_text)
-        #     return ('<p>{}</p>'.format(text))
 
 
 if __name__ == '__main__':
+    # QaCase.count_tag = False
     unittest.main(verbosity=2)
